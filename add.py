be7080cf0389d070645eac516b055b1828341840
Sunny Kumar <sjaishwal@outlook.com>
2023-08-23 01:03:24 +0530

    initial commit

---
+++ add.py
@@ -0,0 +1,9 @@
+class Sum:
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
+        
+    def sum(self):
+        return self.number1 + self.number2
+    
+    
